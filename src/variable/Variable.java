cb0a9e76e4297919252a407505584ed9bc0fd0cc
JunHee Lee <jhlee910609@gmail.com>
2017-07-22 10:59:22 +0900

    initial commit

---
+++ src/variable/Variable.java
@@ -0,0 +1,17 @@
+package variable;
+
+public class Variable {
+
+	public static void main(String[] args) {
+		// 1. 10을 변수 value의 초기값으로 저장 
+		int value = 10;
+		
+		// 2. 변수 value 값을 읽고 10을 더하는 산술 연산 사용
+		// 3. 연산의 결과값을 변수 result의 초기값으로 저장 
+		int result = value + 10;
+		
+		// 4. 변수 result 값을 읽고 console에 출력한다. 
+		System.out.println(result);
+	}
+
+}
