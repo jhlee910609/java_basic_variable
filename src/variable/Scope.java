3ac383b0eb3d6d6769f0a5d49a12205fe737fe46
JunHee Lee <jhlee910609@gmail.com>
2017-07-23 13:28:26 +0900

    modified codes.

--- src/variable/Scope.java
+++ src/variable/Scope.java
@@ -1,10 +1,13 @@
 package variable;
 
 public class Scope {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	// 1. 전역변수 a 선언 
+    int a = 1;
+    
+    public int plus(int c, int d){
+    	int e = 30;
+    	// 2. 전역변수는 아래와 같이 Scope class 코드블록 안에서 언제든 사용할 수 있다.
+    	return c + d + a;
 	}
-
 }
